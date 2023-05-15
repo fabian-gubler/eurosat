e1ae9100e8c29d262ec06eb59b68096cc7932da2
Fabian Gubler <fabian.gubler@hotmail.com>
2023-05-15 13:40:58 +0200

    manual update

--- src/tf_fabian_best_version.py
+++ src/tf_fabian_best_version.py
@@ -9,6 +9,12 @@
 # ---
 
 import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Input, GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
 
 # +
 # Assuming your data is stored in x and y
