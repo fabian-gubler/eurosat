fcd8de58182a3307a7439f571a0058b1c1fbf6c0
Fabian Gubler <fabian.gubler@protonmail.com>
2023-05-15 14:19:20 +0000

    Commit message

--- src/predictions.py
+++ src/predictions.py
@@ -18,15 +18,21 @@ import numpy as np
 import pandas as pd
 
 # Load the model
-name = "resnet50_sd_wo_deeper_batch_128.h5"
+name = "resnet50_std_wo_deeper_batch_128.h5"
 model = tf.keras.models.load_model(name)
 
 # Assume that x_testset is your test dataset
 # And we normalize it in the same way as you did for your training set
 # x_testset = ...
 
+
+
 user = "ubuntu"
-x_testset = np.load(f"/home/{user}/eurosat/preprocessed/x_testset.npy")
+x_testset = np.load(f"/home/{user}/eurosat/preprocessed/x_testset_std.npy")
+
+# Delete B1 (at index 0) and three other bands (let's assume at indices 8, 9, and 10)
+x_testset = np.delete(x_testset, 0, axis=3)
+x_testset = np.delete(x_testset, 8, axis=3)
 
 # Define the classes
 classes = [
