72a84189f6d05019b38e3610883df93352e7c7c1
Fabian Gubler <fabian.gubler@protonmail.com>
2023-05-17 20:47:45 +0200

    manual update

--- src/transform.py
+++ src/transform.py
@@ -1,16 +1,13 @@
 import os
-
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as plt
 
-
-# Source and destination folders
+# Source folder
 src_folder = "/data/eurosat/data/testset/"
-dst_folder = "/data/eurosat/data/testset_rgb/"
 
-# Create the destination folder if it doesn't exist
-os.makedirs(dst_folder, exist_ok=True)
-
+# ImageNet mean and standard deviation
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
 
 def get_rgb_values(filename):
     # Load the image
@@ -18,29 +15,23 @@ def get_rgb_values(filename):
 
     # Reorder the channels to match the RGB order (assuming 0-based indexing)
     rgb_image = multispectral_image[:, :, [3, 2, 1]]
-    print(rgb_image)
 
-    # Normalize the pixel values to the range [0, 255] for each channel separately
-    normalized_image = (
-        (rgb_image - np.min(rgb_image, axis=(0, 1)))
-        / (np.max(rgb_image, axis=(0, 1)) - np.min(rgb_image, axis=(0, 1)))
-    ) * 255
+    # Normalize the pixel values based on ImageNet mean and standard deviation
+    normalized_image = ((rgb_image - np.min(rgb_image, axis=(0,1))) /
+                        (np.max(rgb_image, axis=(0,1)) - np.min(rgb_image, axis=(0,1)))) * 255
 
-    # Convert the image array to 8-bit unsigned integers
-    return normalized_image.astype(np.uint8)
-
+    return normalized_image
 
 # Loop over all files in the source folder
 for filename in os.listdir(src_folder):
-
     # Check if the file is a .npy file
     if filename.endswith(".npy"):
-
         # Get the RGB values for the image
         rgb_image = get_rgb_values(filename)
-        break
 
-        # Save the image to the destination folder
-        # Image.fromarray(rgb_image).save(os.path.join(dst_folder, filename[:-4] + ".png"))
+        print(rgb_image)
 
-
+        # Visualize the image
+        # plt.imshow(rgb_image.astype(np.uint8))
+        # plt.axis('off')
+        # plt.show()
