1e5715fc54924b7bf13cf9d07b86cfec0e985296
Fabian Gubler <fabian.gubler@protonmail.com>
2023-05-17 23:25:08 +0200

    manual

--- src/predictions.py
+++ src/predictions.py
@@ -18,15 +18,19 @@ import numpy as np
 import pandas as pd
 
 # Load the model
-name = "resnet50_std_wo_deeper_rgb.h5"
-model = tf.keras.models.load_model(name)
+# name = "resnet50_std_wo_deeper_rgb.h5"
+# model = tf.keras.models.load_model(name)
 
 # Assume that x_testset is your test dataset
 # And we normalize it in the same way as you did for your training set
 # x_testset = ...
 
-eurosat_dir = "/home/ubuntu"
-x_testset = np.load(f"{eurosat_dir}/preprocessed/x_rgb.npy")
+# eurosat_dir = "/home/ubuntu"
+# x_testset = np.load(f"{eurosat_dir}/preprocessed/x_rgb.npy")
+
+x_testset = np.load("/data/eurosat/data/preprocessed/x_rgb.npy")
+
+print(x_testset.shape)
 
 # Delete B1 (at index 0) and three other bands (let's assume at indices 8, 9, and 10)
 
@@ -48,19 +52,19 @@ classes = [
 ]
 
 # Make predictions
-predictions = model.predict(x_testset)
-
-# Get the class with highest probability for each test image
-predicted_classes = np.argmax(predictions, axis=1)
-
-# Map the class indices to actual class names
-predicted_class_names = [classes[i] for i in predicted_classes]
-
-# Create a DataFrame for the test IDs and their predicted labels
-df = pd.DataFrame(data={
-    'test_id': np.arange(len(predicted_class_names)),
-    'label': predicted_class_names
-})
-
-# Save the DataFrame to a CSV file
-df.to_csv(name+'.csv', index=False)
+# predictions = model.predict(x_testset)
+#
+# # Get the class with highest probability for each test image
+# predicted_classes = np.argmax(predictions, axis=1)
+#
+# # Map the class indices to actual class names
+# predicted_class_names = [classes[i] for i in predicted_classes]
+#
+# # Create a DataFrame for the test IDs and their predicted labels
+# df = pd.DataFrame(data={
+#     'test_id': np.arange(len(predicted_class_names)),
+#     'label': predicted_class_names
+# })
+#
+# # Save the DataFrame to a CSV file
+# df.to_csv(name+'.csv', index=False)
