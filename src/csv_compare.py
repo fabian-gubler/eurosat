caf1557308f8115903a02af838d7e657de29b888
Fabian Gubler <fabian.gubler@protonmail.com>
2023-05-18 06:26:36 +0200

    manual update

---
+++ src/csv_compare.py
@@ -0,0 +1,26 @@
+import csv
+
+def compare_csv_files(file1, file2):
+    count_differing_entries = 0
+    count_total_entries = 0
+
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        csv_reader1 = csv.reader(f1)
+        csv_reader2 = csv.reader(f2)
+
+        for row1, row2 in zip(csv_reader1, csv_reader2):
+            for col1, col2 in zip(row1, row2):
+                if col1 != col2:
+                    count_differing_entries += 1
+                count_total_entries += 1
+
+    percentage_diff = (count_differing_entries / count_total_entries) * 100
+
+    return percentage_diff
+
+# Usage example
+csv_file1 = 'resnet50_std_wo_deeper_rgb.h5.csv'
+csv_file2 = 'efficientnet.csv'
+
+percentage_diff = compare_csv_files(csv_file1, csv_file2)
+print(f"Percentage of differing entries: {percentage_diff:.2f}%")
--- src/model.py
+++ src/model.py
@@ -1,91 +1,144 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.2-dev
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# https://github.com/derevirn/lulc-keras/blob/master/EfficientNet_Finetuned.ipynb
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import tensorflow as tf
-
-from sklearn.model_selection import train_test_split
-from tensorflow.keras import regularizers
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import (Concatenate, Conv2D, Dense, Dropout,
-                                     Flatten, GlobalAveragePooling2D, Input,
-                                     MaxPooling2D)
-from tensorflow.keras.layers.experimental.preprocessing import Resizing
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tqdm.keras import TqdmCallback
-from tensorflow.keras.applications.resnet50 import preprocess_input
-
+from tensorflow.keras import datasets, layers, models, optimizers, callbacks
+from tensorflow.keras.applications import ResNet50, MobileNet
+from efficientnet.tfkeras import EfficientNetB0, EfficientNetB4, EfficientNetB5
 import tensorflow_datasets as tfds
+from sklearn.metrics import classification_report, confusion_matrix
+
+# +
+BATCH_SIZE = 64
+
+ds, ds_info  = tfds.load('eurosat/rgb', with_info=True, split='train')
+figure = tfds.show_examples(ds, ds_info)
+# -
+
+(ds_train, ds_test) = tfds.load('eurosat/rgb',  with_info=False,
+                                         split=['train[:80%]', 'train[80%:]'],
+                                         as_supervised=True)
 
 
-user = "ubuntu"
+# +
+def convert(image, label):
+    image = tf.image.convert_image_dtype(image, tf.float32) # Cast and normalize the image to [0,1]
+    return image, label
 
-print("loading data...")
+def augment(image,label):
+    image,label = convert(image, label)
+    image = tf.image.convert_image_dtype(image, tf.float32) # Cast and normalize the image to [0,1]
+    image = tf.image.rot90(image)
+    image = tf.image.random_hue(image, max_delta=0.2)
+    image = tf.image.random_flip_up_down(image)
+    image = tf.image.random_brightness(image, max_delta=0.5) # Random brightness
+    return image,label
 
-# Assuming your data is stored in x and y
-x = np.load(f"/home/{user}/eurosat/preprocessed/x_std.npy")
-y = np.load(f"/home/{user}/eurosat/preprocessed/y.npy")
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(int(ds_info.splits['train'].num_examples * 0.8))
+ds_train = ds_train.map(augment, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.repeat()
+ds_train = ds_train.batch(BATCH_SIZE)
+# -
 
-x_rgb = x[:,:,:, [3, 2, 1]].copy() # shape: (27000, 64, 64, 3)
+ds_test = ds_test.map(convert, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.cache()
+ds_test = ds_test.batch(BATCH_SIZE)
+
+# +
+base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(64, 64, 3))
+
+#for layer in base_model.layers[:-100]:
+#    layer.trainable = False
+
+#for layer in base_model.layers:
+#    print(layer.name,layer.trainable)
+
+#base_model.trainable = False
+
+# +
+model = models.Sequential()
+model.add(base_model)
+model.add(layers.GlobalMaxPooling2D())
+
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(10, activation = 'softmax'))
+
+model.summary()
+
+steps = int(ds_info.splits['train'].num_examples * 0.8) // BATCH_SIZE
+
+sgd = optimizers.SGD(lr = 1e-03, momentum = 0.9, nesterov = True)
+adam = optimizers.Adam(lr = 1e-03)
+
+early_stop = callbacks.EarlyStopping(monitor = 'val_loss', mode = 'min',
+            patience = 5, restore_best_weights = True, verbose = 1)
+
+reduce_lr = callbacks.ReduceLROnPlateau(monitor = 'val_loss', mode = 'min',
+            patience = 2, factor = 0.5, min_lr = 1e-06, verbose = 1)
+
+model.compile(optimizer=adam, loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(ds_train, validation_data=ds_test, epochs=50,
+                    steps_per_epoch = steps, callbacks=[early_stop, reduce_lr])
 
 
+test_loss, test_acc = model.evaluate(ds_test, verbose=1)
+model.save('models\efficientnet_finetuned')
 
-# Split the dataset into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(x_rgb, y, test_size=0.2, random_state=42)
+# ---
+# Evaluation
+# ---
 
-# Create a custom input layer for the 64x64x20 input
-input_layer = Input(shape=(64, 64, 3))
+fig = plt.figure(figsize=(12,8))
 
-# Add a resizing layer to resize the input to the size ResNet expects
-resizing_layer = Resizing(224, 224, interpolation="Bilinear")(input_layer)
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
 
-# Load the ResNet101 model without the top classification layer and with custom input
-base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=resizing_layer)
+plt.show()
 
-# Add a custom classification layer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)  # additional fully-connected layer
-x = Dropout(0.2)(x)  # dropout for regularization, 0.1-0.5, start small
-x = Dense(1024, activation='relu')(x)  # additional fully-connected layer
-x = Dropout(0.2)(x)  # dropout for regularization, 0.1-0.5, start small
-#x = Dense(1024, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01))(x)  # L2 regularization with a factor of 0.01, 0.0001 to 0.1, start small
+fig = plt.figure(figsize=(12,8))
 
-predictions = Dense(y.shape[1], activation='softmax')(x)
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label = 'val_loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
 
-# Freeze all layers in the base model
-for layer in base_model.layers:
-    layer.trainable = False
+plt.show()
 
-# Unfreeze the last two residual blocks (9 layers)
-for layer in base_model.layers[-9:]:
-    layer.trainable = True
+from utils import plot_confusion_matrix
 
-# Create the final model
-model = Model(inputs=input_layer, outputs=predictions)
+predictions = []
+labels = []
+for test_images, test_labels in ds_test:
+  probabilities = model(test_images.numpy())
+  predictions.extend(tf.argmax(probabilities, axis=1))
+  labels.extend(test_labels.numpy())
 
-# Compile the model
-model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.003), loss='categorical_crossentropy', metrics=['accuracy'])
+classes = ds_info.features['label'].names
 
-# Create data augmentation generator
-datagen = ImageDataGenerator(
-    rotation_range=20,
-    shear_range=0.2,  # added shear transformation
-    #width_shift_range=0.2,
-    #height_shift_range=0.2,
-    #horizontal_flip=True,
-    #vertical_flip=True,
-    #zoom_range=0.2,  # added zoom
-)
-
-# Define the checkpoint callback
-checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath='resnet50_std_rgb_only.h5', monitor='val_accuracy', mode='max', save_best_only=True, save_weights_only=False, verbose=1)
-# Define the early stopping callback
-early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', patience=5, verbose=1, restore_best_weights=True)
-
-# Fit the model with the augmented data
-batch_size = 50
-epochs = 20
-model.fit(datagen.flow(x_train, y_train, batch_size=batch_size),
-          steps_per_epoch=len(x_train) // batch_size,
-          validation_data=(x_test, y_test),
-          epochs=epochs,
-          callbacks=[checkpoint_callback, early_stopping_callback])  
+plot_confusion_matrix(labels, predictions, classes, normalize=True)
