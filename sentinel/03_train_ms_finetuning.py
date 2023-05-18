79bf71aaa920262f7d18c3e54465c4ad2c9ca2ae
Fabian Gubler <fabian.gubler@protonmail.com>
2023-05-18 11:42:27 +0200

    manual update

--- sentinel/03_train_ms_finetuning.py
+++ sentinel/03_train_ms_finetuning.py
@@ -43,7 +43,7 @@ x = np.delete(x, 12, axis=3)
 
 x = np.delete(x, 13, axis=3)
 x = np.delete(x, 14, axis=3)
-x = np.delete(x, 15, axis=3)
+# x = np.delete(x, 15, axis=3)
 # x = np.delete(x, 16, axis=3)
 # x = np.delete(x, 17, axis=3)
 # x = np.delete(x, 18, axis=3)
