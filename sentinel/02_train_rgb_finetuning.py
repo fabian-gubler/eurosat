af4b754355526f95968cd7b02077e995ca436e02
Fabian Gubler <fabian.gubler@protonmail.com>
2023-05-18 07:48:48 +0200

    manual update

--- sentinel/02_train_rgb_finetuning.py
+++ sentinel/02_train_rgb_finetuning.py
@@ -11,6 +11,7 @@ License: MIT
 """
 import os
 
+from image_functions import preprocessing_image_rgb
 from tensorflow.keras.applications.densenet import DenseNet201 as DenseNet
 from tensorflow.keras.applications.vgg16 import VGG16 as VGG
 from tensorflow.keras.callbacks import (EarlyStopping, ModelCheckpoint,
@@ -19,9 +20,6 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-from image_functions import preprocessing_image_rgb
-
 
 # variables
 path_to_split_datasets = "../data/AllRGB"
@@ -35,19 +33,20 @@ path_to_train = os.path.join(path_to_split_datasets, "train")
 path_to_validation = os.path.join(path_to_split_datasets, "validation")
 
 # get number of classes
-sub_dirs = [sub_dir for sub_dir in os.listdir(path_to_train)
-            if os.path.isdir(os.path.join(path_to_train, sub_dir))]
+sub_dirs = [
+    sub_dir
+    for sub_dir in os.listdir(path_to_train)
+    if os.path.isdir(os.path.join(path_to_train, sub_dir))
+]
 num_classes = len(sub_dirs)
 
 # parameters for CNN
 if use_vgg:
-    base_model = VGG(include_top=False,
-                     weights='imagenet',
-                     input_shape=(64, 64, 3))
+    base_model = VGG(include_top=False, weights="imagenet", input_shape=(64, 64, 3))
 else:
-    base_model = DenseNet(include_top=False,
-                          weights='imagenet',
-                          input_shape=(64, 64, 3))
+    base_model = DenseNet(
+        include_top=False, weights="imagenet", input_shape=(64, 64, 3)
+    )
 # add a global spatial average pooling layer
 top_model = base_model.output
 top_model = GlobalAveragePooling2D()(top_model)
@@ -57,10 +56,10 @@ top_model = GlobalAveragePooling2D()(top_model)
 if use_vgg:
     # only in VGG19 a fully connected nn is added for classfication
     # DenseNet tends to overfitting if using additionally dense layers
-    top_model = Dense(2048, activation='relu')(top_model)
-    top_model = Dense(2048, activation='relu')(top_model)
+    top_model = Dense(2048, activation="relu")(top_model)
+    top_model = Dense(2048, activation="relu")(top_model)
 # and a logistic layer
-predictions = Dense(num_classes, activation='softmax')(top_model)
+predictions = Dense(num_classes, activation="softmax")(top_model)
 
 # this is the model we will train
 model = Model(inputs=base_model.input, outputs=predictions)
@@ -75,17 +74,16 @@ train_datagen = ImageDataGenerator(
     rotation_range=45,
     horizontal_flip=True,
     vertical_flip=True,
-    preprocessing_function=preprocessing_image_rgb)
+    preprocessing_function=preprocessing_image_rgb,
+)
 
 # ... initialization for validation
-test_datagen = ImageDataGenerator(
-    preprocessing_function=preprocessing_image_rgb)
+test_datagen = ImageDataGenerator(preprocessing_function=preprocessing_image_rgb)
 
 # ... definition for training
-train_generator = train_datagen.flow_from_directory(path_to_train,
-                                                    target_size=(64, 64),
-                                                    batch_size=batch_size,
-                                                    class_mode='categorical')
+train_generator = train_datagen.flow_from_directory(
+    path_to_train, target_size=(64, 64), batch_size=batch_size, class_mode="categorical"
+)
 # just for information
 class_indices = train_generator.class_indices
 print(class_indices)
@@ -95,7 +93,8 @@ validation_generator = test_datagen.flow_from_directory(
     path_to_validation,
     target_size=(64, 64),
     batch_size=batch_size,
-    class_mode='categorical')
+    class_mode="categorical",
+)
 
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional layers
@@ -103,8 +102,11 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='adadelta', loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(
+    optimizer="adadelta",
+    loss="categorical_crossentropy",
+    metrics=["categorical_accuracy"],
+)
 
 # generate callback to save best model w.r.t val_categorical_accuracy
 if use_vgg:
@@ -112,32 +114,37 @@ if use_vgg:
 else:
     file_name = "dense"
 
-checkpointer = ModelCheckpoint("../data/models/" + file_name +
-                               "_rgb_transfer_init." +
-                               "{epoch:02d}-{val_accuracy:.3f}." +
-                               "hdf5",
-                               monitor='val__accuracy',
-                               verbose=1,
-                               save_best_only=True,
-                               mode='max')
+# checkpointer = ModelCheckpoint("../data/models/" + file_name +
+#                                "_rgb_transfer_init." +
+#                                "{epoch:02d}-{val_categorical_accuracy:.3f}." +
+#                                "hdf5",
+#                                monitor='val_categorical_accuracy',
+#                                verbose=1,
+#                                save_best_only=True,
+#                                mode='max')
+#
+# earlystopper = EarlyStopping(monitor='val_categorical_accuracy',
+#                              patience=10,
+#                              mode='max',
+#                              restore_best_weights=True)
 
-earlystopper = EarlyStopping(monitor='val_accuracy',
-                             patience=10,
-                             mode='max',
-                             restore_best_weights=True)
-
-tensorboard = TensorBoard(log_dir='./logs', write_graph=True,
-                          write_images=True, update_freq='epoch')
+tensorboard = TensorBoard(
+    log_dir="./logs", write_graph=True, write_images=True, update_freq="epoch"
+)
 
 history = model.fit(
     train_generator,
     steps_per_epoch=50,
     epochs=10000,
-    callbacks=[checkpointer, earlystopper,
-                tensorboard],
+    callbacks=[
+        # checkpointer,
+        # earlystopper,
+        tensorboard
+    ],
     validation_data=validation_generator,
-    validation_steps=500)
-initial_epoch = len(history.history['loss'])+1
+    validation_steps=500,
+)
+initial_epoch = len(history.history["loss"]) + 1
 # at this point, the top layers are well trained and we can start fine-tuning
 # convolutional layers. We will freeze the bottom N layers
 # and train the remaining top layers.
@@ -164,31 +171,40 @@ else:
 
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(
+    optimizer=SGD(lr=0.0001, momentum=0.9),
+    loss="categorical_crossentropy",
+    metrics=["categorical_accuracy"],
+)
 
 # generate callback to save best model w.r.t val_categorical_accuracy
 if use_vgg:
     file_name = "vgg"
 else:
     file_name = "dense"
-checkpointer = ModelCheckpoint("../data/models/" + file_name +
-                               "_rgb_transfer_final." +
-                               "{epoch:02d}-{val_accuracy:.3f}" +
-                               ".hdf5",
-                               monitor='val_accuracy',
-                               verbose=1,
-                               save_best_only=True,
-                               mode='max')
-earlystopper = EarlyStopping(monitor='val_accuracy',
-                             patience=50,
-                             mode='max')
+checkpointer = ModelCheckpoint(
+    "../data/models/"
+    + file_name
+    + "_rgb_transfer_final."
+    + "{epoch:02d}-{val_categorical_accuracy:.3f}"
+    + ".hdf5",
+    monitor="val_categorical_accuracy",
+    verbose=1,
+    save_best_only=True,
+    mode="max",
+)
+earlystopper = EarlyStopping(
+    monitor="val_categorical_accuracy", patience=50, mode="max"
+)
 model.fit(
     train_generator,
     steps_per_epoch=50,
     epochs=10000,
-    callbacks=[checkpointer, earlystopper, tensorboard],
+    callbacks=[
+        # checkpointer, 
+        # earlystopper, 
+        tensorboard],
     validation_data=validation_generator,
     validation_steps=500,
-    initial_epoch=initial_epoch)
+    initial_epoch=initial_epoch,
+)
